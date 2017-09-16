484861c3e93df1d9a2dc4ca7095048a681208f6c
Max Langer <maxlanger@me.com>
2017-09-16 10:18:41 +0200

    Initial Commit

---
+++ StackOverflow ToolbarItem Test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Max Langer on 09.09.17.
+//  Copyright © 2017 Max Langer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
